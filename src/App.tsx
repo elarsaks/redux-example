de98df565b3761c4cdd3f85dd1b7f83953be4073
elarsaks <elarsaks@gmail.com>
2021-04-21 09:08:43 +0300

    Init project.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const App: React.FC = () => {
+  return (
+    <main>
+        <div >
+          <h1>
+          Droppe Xmas
+          </h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
