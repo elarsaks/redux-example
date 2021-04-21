c704adcfdbdf89f6e220b15464923351df29dae8
elarsaks <elarsaks@gmail.com>
2021-04-21 13:19:27 +0300

    Add styled components.

--- src/components/Right.tsx
+++ src/components/Right.tsx
@@ -1,5 +1,25 @@
 import * as React from "react"
+import styled from 'styled-components'
 import { useSelector, shallowEqual } from "react-redux"
+
+const RightDiv = styled.div`
+  width: 45%;
+  background-color: aqua;
+`
+
+const WishList = styled.div`
+  height: 1.2em;
+  width: 100%;
+  background-color: gray;
+`
+
+
+const WishListItem = styled.div`
+  height: 1.2em;
+  width: 100%;
+  background-color: #808080;
+`
+
 
 export const Right: React.FC = () => {
 
@@ -9,8 +29,21 @@ export const Right: React.FC = () => {
   )
 
   return (
-    <div className="half right">
+    <RightDiv>
+      {shoppingList.map((wishList: WishList) => (
+        <WishList key={wishList.name}>
+          <h3>{wishList.name}</h3>
 
-    </div>
+        </WishList>
+      ))}
+    </RightDiv>
   )
 }
+
+/* 
+          {wishList.items.map((wishListItem: any) => (
+            <WishListItem key={wishListItem.id}>
+              <p>{ wishListItem}</p>
+            </WishListItem>
+          ))}
+*/
--- src/store/actions.tsx
+++ src/store/actions.tsx
@@ -48,6 +48,5 @@ export const setInitialState = () => (dispatch: any) => {
     
   getProductsList()
     .then(data => dispatch(setShoppingList(data)))
-
 }
 
